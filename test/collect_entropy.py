ef7dd26ac384c43f1b474f66068db902e86e6e52
sokrasins <sokrasins@gmail.com>
2021-07-24 14:33:14 -0400

    Add latest rev and entropy collection script

---
+++ test/collect_entropy.py
@@ -0,0 +1,223 @@
+"""
+Demonstrates how to stream using a callback to read stream, which is useful
+for streaming in external clock stream mode.
+
+Relevant Documentation:
+ 
+LJM Library:
+    LJM Library Installer:
+        https://labjack.com/support/software/installers/ljm
+    LJM Users Guide:
+        https://labjack.com/support/software/api/ljm
+    Opening and Closing:
+        https://labjack.com/support/software/api/ljm/function-reference/opening-and-closing
+    Utility Functions (such as NamesToAddresses):
+        https://labjack.com/support/software/api/ljm/function-reference/utility
+    Stream Functions:
+        https://labjack.com/support/software/api/ljm/function-reference/stream-functions
+ 
+T-Series and I/O:
+    Modbus Map:
+        https://labjack.com/support/software/api/modbus/modbus-map
+    Stream Mode: 
+        https://labjack.com/support/datasheets/t-series/communication/stream-mode
+    Analog Inputs:
+        https://labjack.com/support/datasheets/t-series/ain
+    Stream-Out: 
+        https://labjack.com/support/datasheets/t-series/communication/stream-mode/stream-out/stream-out-description
+    Digital I/O:
+        https://labjack.com/support/datasheets/t-series/digital-io
+    Pulse Out:
+        https://labjack.com/support/datasheets/t-series/digital-io/extended-features/pulse-out
+    Stream Mode (externally clocked):
+        https://labjack.com/support/datasheets/t-series/communication/stream-mode#externally-clocked
+
+"""
+from datetime import datetime
+import sys
+import threading
+import time
+import traceback
+
+from labjack import ljm
+
+
+# Class to hold our stream information
+class StreamInfo:
+    def __init__(self):
+        self.handle = 0
+        self.scanRate = 0
+        self.scansPerRead = 0
+        self.streamLengthMS = 0
+        self.done = False
+
+        self.numAddresses = 0
+        self.aScanList = 0
+        self.aScanListNames = 0
+        
+        self.aDataSize = 0
+        self.aData = None
+
+        self.streamRead = 0
+        self.totSkip = 0
+        self.totScans = 0
+
+        self.bin_samps = []
+
+
+printLock = threading.Lock()
+def printWithLock(string):
+    global printLock
+    with printLock:
+        print(string)
+
+
+# Function to pass to the callback function. This needs have one
+# parameter/argument, which will be the handle.
+def myStreamReadCallback(arg):
+    global si
+
+    if si.handle != arg:
+        printWithLock("myStreamReadCallback - Unexpected argument: %d" % (arg))
+        return
+
+    # Check if stream is done so that we don't output the print statement below.
+    if si.done:
+        return
+
+    string = "\niteration: %3d\n" % si.streamRead
+    si.streamRead += 1
+
+    try:
+        ret = ljm.eStreamRead(si.handle)
+        si.aData = ret[0]
+        deviceScanBacklog = ret[1]
+        ljmScanBackLog = ret[2]
+
+        # Extract the data from FIO4
+        si.bin_samps += list(map(lambda x : int(x) >> 4 & 1, si.aData))
+
+        scans = len(si.aData) / si.numAddresses
+        si.totScans += scans
+
+        # Count the skipped samples which are indicated by -9999 values. Missed
+        # samples occur after a device's stream buffer overflows and are
+        # reported after auto-recover mode ends.
+        curSkip = si.aData.count(-9999.0)
+        si.totSkip += curSkip
+
+        string = "  Total samples: %d" % (len(si.bin_samps))
+        string += "\n  Scans Skipped = %0.0f, Scan Backlogs: Device = %i, LJM = %i" % \
+                (curSkip/si.numAddresses, deviceScanBacklog, ljmScanBackLog)
+        printWithLock(string)
+
+    # If LJM has called this callback, the data is valid, but LJM_eStreamRead
+    # may return LJME_STREAM_NOT_RUNNING if another thread (such as the Python
+    # main thread) has stopped stream.
+    except ljm.LJMError as err:
+        if err.errorCode == ljm.errorcodes.STREAM_NOT_RUNNING:
+            printWithLock("eStreamRead returned LJME_STREAM_NOT_RUNNING.")
+        else:
+            printWithLock(err)
+
+
+def reduce_bin_to_byte(bin_list):
+    bin_chunks = [bin_list[i:i+8] for i in range(0, len(bin_list), 8)]
+
+    byte_array = []
+    for chunk in bin_chunks:
+        byte_val = 0
+        for place, bin_val in enumerate(chunk):
+            byte_val += bin_val << (7-place)
+        byte_array.append(byte_val)
+
+    return byte_array
+
+
+# Create the global StreamInfo class which is used to pass information between
+# the callback and main code.
+si = StreamInfo()
+
+
+if __name__ == "__main__":
+    # Open first found T4 LabJack
+    handle = ljm.openS("T4", "ANY", "ANY")  # Any device, Any connection, Any identifier
+
+    info = ljm.getHandleInfo(handle)
+    print("Opened a LabJack with Device type: %i, Connection type: %i,\n"
+          "Serial number: %i, IP address: %s, Port: %i,\nMax bytes per MB: %i" %
+          (info[0], info[1], info[2], ljm.numberToIP(info[3]), info[4], info[5]))
+
+    deviceType = info[0]
+
+    # Disable scaling of scream to analog range
+    ljm.writeLibraryConfigS("LJM_STREAM_AIN_BINARY", 1)
+
+
+    # Stream Configuration
+    si.aScanListNames = ["FIO_STATE"]
+    si.numAddresses = len(si.aScanListNames)
+    si.aScanList = ljm.namesToAddresses(si.numAddresses, si.aScanListNames)[0]
+    si.scanRate = 20000
+    si.scansPerRead = int(si.scanRate / 2)
+
+    si.numberOfReadsToPerform = int(100000000 / si.scansPerRead)
+    si.done = False
+    si.aDataSize = si.numAddresses * si.scansPerRead
+    si.handle = handle
+
+    try:
+        # When streaming, negative channels and ranges can be configured for
+        # individual analog inputs, but the stream has only one settling time
+        # and resolution.
+
+        # LabJack T4 configuration
+        t0 = datetime.now()
+
+        # Configure and start stream
+        si.scanRate = ljm.eStreamStart(handle, si.scansPerRead, si.numAddresses, si.aScanList, si.scanRate)
+        print("\nStream started with a scan rate of %0.0f Hz." % si.scanRate)
+
+        
+        # Set the callback function.
+        ljm.setStreamCallback(handle, myStreamReadCallback)
+
+        printWithLock("Stream running and callback set.")
+        printWithLock("Waiting until eStreamRead has been called %d times."
+            % si.numberOfReadsToPerform)
+
+        while (si.streamRead < si.numberOfReadsToPerform):
+            time.sleep(.1)
+
+        t1 = datetime.now()
+
+        printWithLock("\nStreaming done. %.3f milliseconds have elapsed since eStreamStart" %
+              ((t1-t0).seconds*1000 + float((t1-t0).microseconds)/1000))
+
+    except ljm.LJMError:
+        ljme = sys.exc_info()[1]
+        printWithLock(ljme)
+    except Exception:
+        e = sys.exc_info()[1]
+        printWithLock(e)
+
+    try:
+        printWithLock("\nStop Stream")
+        si.done = True
+        ljm.eStreamStop(handle)
+    except ljm.LJMError:
+        ljme = sys.exc_info()[1]
+        print(ljme)
+    except Exception:
+        e = sys.exc_info()[1]
+        print(e)
+
+    # Close handle
+    ljm.close(handle)
+
+    # Write the bin data to a file here!
+    byte_data = bytearray(reduce_bin_to_byte(si.bin_samps))
+
+    with open('rand_samp.bin', 'wb') as f:
+        f.write(byte_data)
+
